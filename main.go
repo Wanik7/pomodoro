85f10d77a60dbc15b3251fe092b513fd63819ac7
Wanik7 <wanikuteikin@mail.ru>
2025-11-20 21:25:16 +0300

    v 0.1 release

---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+const (
+	workMode mode = iota
+	breakMode
+)
+
+type mode int
+
+type tickMsg struct{}
+
+type model struct {
+	mode          mode
+	workDuration  int
+	breakDuration int
+	secondsLeft   int
+	running       bool
+	quitting      bool
+}
+
+func initialModel() model {
+	const workTime int = 15
+	const breakTime int = 5
+	return model{
+		mode:          workMode,
+		workDuration:  workTime,
+		breakDuration: breakTime,
+		secondsLeft:   workTime,
+		running:       true,
+		quitting:      false,
+	}
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(time.Second, func(time.Time) tea.Msg {
+		return tickMsg{}
+	})
+}
+
+func (m model) currentDuration() int {
+	if m.mode == workMode {
+		return m.workDuration
+	}
+	return m.breakDuration
+}
+
+func (m *model) switchMode() {
+	if m == nil {
+		log.Fatal("model is nil")
+	}
+	if m.mode == workMode {
+		m.mode = breakMode
+		m.secondsLeft = m.breakDuration
+	} else {
+		m.mode = workMode
+		m.secondsLeft = m.workDuration
+	}
+}
+
+func (m model) Init() tea.Cmd { return tickCmd() }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tickMsg:
+		if m.running && m.secondsLeft > 0 {
+			m.secondsLeft--
+			if m.secondsLeft == 0 {
+				m.switchMode()
+			}
+		}
+		return m, tickCmd()
+
+	case tea.KeyMsg:
+
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.quitting = true
+			return m, tea.Quit
+
+		case "p":
+			if m.secondsLeft > 0 {
+				m.running = !m.running
+			}
+			return m, nil
+
+		case "r":
+			m.secondsLeft = m.currentDuration()
+			m.running = true
+			return m, nil
+
+		case "m":
+			m.switchMode()
+			m.running = true
+			return m, nil
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	mm := m.secondsLeft / 60
+	ss := m.secondsLeft % 60
+	status := "running"
+	modeStr := "work"
+	if m.mode == breakMode {
+		modeStr = "break"
+	}
+	if !m.running {
+		status = "not running"
+	}
+	if m.quitting {
+		return "Bye!"
+	} else {
+		return fmt.Sprintf("\n		Pomodoro\n\nMode: %s 	Time left: %d%d (%s)\n\nPress 'p' to pause,"+
+			" 'r' to reset, 'm' to change mode\n\n	Press 'q'/'ctrl+c' to quit", modeStr, mm, ss, status)
+	}
+}
+
+func main() {
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
